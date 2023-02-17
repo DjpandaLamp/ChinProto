dc44e5f2bf5d44ed25075377ebf62dc120d0d561
DjpandaLamp <68932550+DjpandaLamp@users.noreply.github.com>
2023-02-16 17:41:34 -0800

    Added Startline script
    I also made a new track for testing the rebuilt the scripts

--- New Game/StartLine.cs
+++ New Game/StartLine.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Pathfinding.Ionic.Zip;
 using UnityEngine;
 
 public class StartLine : MonoBehaviour
 {
+    private LevelDefine define;
+    public string s1;
+    public string s2;
+    public int gameNum;
     // Start is called before the first frame update
     void Start()
     {
-        
+        define = GameObject.FindGameObjectWithTag("leveldefine").GetComponent<LevelDefine>();
+        s1 = this.gameObject.name;
+        s2 = s1.Substring(s1.Length - 1);
+        gameNum = int.Parse(s2);
     }
 
     // Update is called once per frame
